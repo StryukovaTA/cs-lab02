4a753dbb8c690e32fd2898f29eb71cc330224e2b
alice (StryukovaTA) <alice@example.com>
2019-03-19 02:31:00 +0300

    code: добавили вывод максимума 2 чисел

--- main.cpp
+++ main.cpp
@@ -10,5 +10,14 @@ int main()
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << "Max = ";
+    if (a > b)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << b;
+    }
 }
